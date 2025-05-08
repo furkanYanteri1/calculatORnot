9b3c7e7789f47da8fa589a610749375afbcc7f08
Furkan Yanteri <furkanyanteri@gmail.com>
2025-05-08 17:59:49 +0300

    Initial Commit

---
+++ calculatORnot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calculatORnot
+//
+//  Created by Furkan Yanteri on 8.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calculatORnot/calculatORnotApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatORnotApp.swift
+//  calculatORnot
+//
+//  Created by Furkan Yanteri on 8.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct calculatORnotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
