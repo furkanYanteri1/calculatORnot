639f4c40739ee65dbf01d6c372951df4ef69171e
Furkan Yanteri <furkanyanteri@gmail.com>
2025-05-10 17:36:04 +0300

    feat: model and view for numbers, operations added

--- calculatORnot/Model/calculatorModel.swift
+++ calculatORnot/Model/calculatorModel.swift
@@ -4,4 +4,42 @@
 //
 //  Created by Furkan Yanteri on 8.05.2025.
 //
+import SwiftUI
 
+
+enum Keys: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case subtract = "-"
+    case divide = "÷"
+    case multiply = "x"
+    case equal = " ="
+    case clear = "AC"
+    case decimal = "."
+    case percent = "%"
+    case negative = "-/+"
+    
+    var buttonColor:Color {
+        switch self {
+        case .add,.subtract,.multiply,.divide,.equal:
+            return Color("Voperator")
+        case .clear,.negative,.percent:
+            return Color("Hoperator")
+        default:
+            return Color("Number")
+        }
+    }
+}
+ 
+enum Operation {
+    case add, subtract, divide, multiply, none
+}
---
+++ calculatORnot/Views/KeyVİew.swift
@@ -0,0 +1,25 @@
+//
+//  KeyVİew.swift
+//  calculatORnot
+//
+//  Created by Furkan Yanteri on 10.05.2025.
+//
+
+import SwiftUI
+
+struct KeyVI_ew: View {
+    let buttons: [[Keys]] = [
+        [Keys.clear, Keys.negative, Keys.percent, Keys.divide],
+        [Keys.seven, Keys.eight, Keys.nine, Keys.multiply],
+        [Keys.four, Keys.five, Keys.six, Keys.subtract],
+        [Keys.one, Keys.two, Keys.three, Keys.add],
+        [Keys.zero, Keys.decimal, Keys.equal,]
+    ]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KeyVI_ew()
+}
