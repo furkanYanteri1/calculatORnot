2d77057fc453dcf52c5c900c6bbc3e6d1b4b4065
Furkan Yanteri <furkanyanteri@gmail.com>
2025-05-10 19:30:42 +0300

    chore: type convention

--- calculatORnot/Views/KeyVİew.swift
+++ calculatORnot/Views/KeyVİew.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct KeyVI_ew: View {
     let buttons: [[Keys]] = [
-        [Keys.clear, Keys.negative, Keys.percent, Keys.divide],
-        [Keys.seven, Keys.eight, Keys.nine, Keys.multiply],
-        [Keys.four, Keys.five, Keys.six, Keys.subtract],
-        [Keys.one, Keys.two, Keys.three, Keys.add],
-        [Keys.zero, Keys.decimal, Keys.equal]
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal]
     ]
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
