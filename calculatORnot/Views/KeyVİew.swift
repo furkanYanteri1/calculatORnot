f39f206080f599c330d8fb5e5c79a6014a58c5d5
Furkan Yanteri <furkanyanteri@gmail.com>
2025-05-10 17:44:39 +0300

    fix: typo in array decleration

--- calculatORnot/Views/KeyVİew.swift
+++ calculatORnot/Views/KeyVİew.swift
@@ -13,7 +13,7 @@ struct KeyVI_ew: View {
         [Keys.seven, Keys.eight, Keys.nine, Keys.multiply],
         [Keys.four, Keys.five, Keys.six, Keys.subtract],
         [Keys.one, Keys.two, Keys.three, Keys.add],
-        [Keys.zero, Keys.decimal, Keys.equal,]
+        [Keys.zero, Keys.decimal, Keys.equal]
     ]
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
